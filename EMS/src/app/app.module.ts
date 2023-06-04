30de75d584d4fa460d9b7e37afa4b046c3042ae1
Rishi Kumar <rishi@botlie.com>
2023-06-04 08:24:38 +0530

    c

--- EMS/src/app/app.module.ts
+++ EMS/src/app/app.module.ts
@@ -1,14 +1,123 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import {LayoutModule} from '@angular/cdk/layout';
+import {CdkListboxModule} from '@angular/cdk/listbox';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { BreakpointObserverOverviewComponent } from './Extra/breakpoint-observer-overview/breakpoint-observer-overview.component';
+import {ObserversModule} from '@angular/cdk/observers';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { SignUPComponent } from './Pages/sign-up/sign-up.component';
+import { LoginComponent } from './Pages/login/login.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatBottomSheetModule} from '@angular/material/bottom-sheet';
+import {MatButtonModule} from '@angular/material/button';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatCardModule} from '@angular/material/card';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatInputModule} from '@angular/material/input';
+import {MatListModule} from '@angular/material/list';
+import {MatNativeDateModule, MatRippleModule} from '@angular/material/core';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSelectModule} from '@angular/material/select';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatSortModule} from '@angular/material/sort';
+import {MatTableModule} from '@angular/material/table';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatTreeModule} from '@angular/material/tree';
+import { ForgetPasswordComponent } from './Pages/forget-password/forget-password.component';
+import { DeleteWarningComponent } from './Popup/delete-warning/delete-warning.component';
+import { UpdateDetailComponent } from './Popup/update-detail/update-detail.component';
+import { AddEmployeeComponent } from './Employee/add-employee/add-employee.component';
+import { ListEmployeeComponent } from './Employee/list-employee/list-employee.component';
+import { RegiSuccesfulComponent } from './Popup/regi-succesful/regi-succesful.component';
+import { TermsConditionComponent } from './Extra/terms-condition/terms-condition.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FooterComponent,
+    BreakpointObserverOverviewComponent,
+    SignUPComponent,
+    LoginComponent,
+    ForgetPasswordComponent,
+    DeleteWarningComponent,
+    UpdateDetailComponent,
+    AddEmployeeComponent,
+    ListEmployeeComponent,
+    RegiSuccesfulComponent,
+    TermsConditionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    LayoutModule,
+    CdkListboxModule,
+    ObserversModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatGridListModule,
+    MatToolbarModule,
+    MatIconModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatSidenavModule,
+    MatAutocompleteModule,
+    MatBadgeModule,
+    MatBottomSheetModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatStepperModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatInputModule,
+    MatListModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatTableModule,
+    MatTabsModule,
+    MatTooltipModule,
+    MatTreeModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
