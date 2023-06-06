0c5e1d549d450349380f83887c6b9bfee6c5c682
Rishi Kumar <rishi@botlie.com>
2023-06-07 04:58:43 +0530

    c

--- EMS/src/app/Popup/update-detail/update-detail.component.ts
+++ EMS/src/app/Popup/update-detail/update-detail.component.ts
@@ -1,6 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
+import { ApiService } from 'src/app/Services/api.service';
 
 @Component({
   selector: 'app-update-detail',
@@ -11,9 +13,10 @@ export class UpdateDetailComponent implements OnInit{
 
   updateForm!:FormGroup;
 
-  constructor(public dialogRef: MatDialogRef<UpdateDetailComponent>,){}
+  constructor(public api: ApiService,private Http:HttpClient ,public dialogRef: MatDialogRef<UpdateDetailComponent>,){}
 
   ngOnInit(): void {
+    this.getEmployee();
      
     this.updateForm = new FormGroup({
       id: new FormControl('', [Validators.required,]),
@@ -24,6 +27,11 @@ export class UpdateDetailComponent implements OnInit{
 
     })
   }
+
+  getEmployee() {
+    this.Http.get("http://localhost:8080/getEmpAll").subscribe((response: any) => { return response;}
+  )};
+
   onClose(){
     this.dialogRef.close();
   }
@@ -33,7 +41,17 @@ export class UpdateDetailComponent implements OnInit{
     this.updateForm.reset();
   }
   onDelete(){
-    
+    var employee = this.updateForm.value;
+    if(this.updateForm.invalid){
+      return;
+    }else{
+       this.api.updateEmployee(employee).subscribe((res:any) => {
+         alert("data has been updated");
+         return res;
+        })
+        this.getEmployee();
+        this.dialogRef.close();
+    }
   }
 
 }
